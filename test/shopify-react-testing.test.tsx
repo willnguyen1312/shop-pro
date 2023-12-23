c171b3e98021f714cadbb8573813d182c26cf45b
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-12-23 08:52:07 -0400

    Leverage Vitest fake timer 🚀

---
+++ test/shopify-react-testing.test.tsx
@@ -0,0 +1,40 @@
+import { expect, test, vi } from "vitest";
+import { mount } from "@shopify/react-testing";
+import React from "react";
+
+const App = () => {
+  const [counter, setCounter] = React.useState(0);
+
+  const asyncIncrement = async () => {
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        setCounter(counter + 1);
+        resolve(void 0);
+      }, 100);
+    });
+  };
+
+  return (
+    <>
+      <h1>Value: {counter}</h1>
+      <button onClick={asyncIncrement}>Increment</button>
+    </>
+  );
+};
+
+test("it should pass", async () => {
+  vi.useFakeTimers();
+  const wrapper = mount(<App />);
+
+  const incrementButton = wrapper.find("button");
+  const textElement = wrapper.find("h1");
+
+  incrementButton?.trigger("onClick");
+
+  expect(textElement?.text()).toBe("Value: 0");
+
+  // Flush all timers with vitest
+  await vi.runAllTimersAsync();
+
+  expect(textElement?.text()).toBe("Value: 1");
+});
